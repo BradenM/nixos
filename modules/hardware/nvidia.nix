d7b5f987e634b397e95c8d40f6814aa1711faf51
Braden Mars <bradenmars@bradenmars.me>
2025-12-08 01:39:13 -0600

    feat(modules/hardware/nvidia): add nvidia hardware module
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ modules/hardware/nvidia.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+{
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    nvidiaSettings = true;
+  };
+
+  environment.sessionVariables = {
+    # Wayland support for various toolkits
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    QT_QPA_PLATFORM = "wayland";
+    SDL_VIDEODRIVER = "wayland";
+    CLUTTER_BACKEND = "wayland";
+
+    # NVIDIA-specific Wayland settings
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    LIBVA_DRIVER_NAME = "nvidia";
+
+    # fix cursor issues on wlroots compositors
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
+}
