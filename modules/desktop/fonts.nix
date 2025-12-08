98edcb35dc7f085a0ea5066ec60559c8d3d10e2c
Braden Mars <bradenmars@bradenmars.me>
2025-12-08 15:34:00 -0600

    feat(modules/desktop): add basic fonts module
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ modules/desktop/fonts.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      font-awesome
+      liberation_ttf
+      source-code-pro
+    ];
+
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "JetBrainsMono Nerd Font" "Noto Sans Mono" ];
+        sansSerif = [ "Noto Sans" ];
+        serif = [ "Noto Serif" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
+}
