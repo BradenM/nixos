9d59146184960f0dfc16d2fd9005ac16c8df2ef4
Braden Mars <bradenmars@bradenmars.me>
2025-12-08 18:16:27 -0600

    feat(home/desktop/mako): update mako options
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- home/desktop/mako.nix
+++ home/desktop/mako.nix
@@ -4,30 +4,33 @@
   services.mako = {
     enable = true;
 
-    font = "JetBrainsMono Nerd Font 10";
-    width = 350;
-    height = 150;
-    margin = "10";
-    padding = "15";
-    borderSize = 2;
-    borderRadius = 8;
+    settings = {
+      font = "JetBrainsMono Nerd Font 10";
+      width = 350;
+      height = 150;
+      margin = "10";
+      padding = "15";
+      border-size = 2;
+      border-radius = 8;
 
-    backgroundColor = "#1e1e2e";
-    textColor = "#cdd6f4";
-    borderColor = "#89b4fa";
+      background-color = "#1e1e2e";
+      text-color = "#cdd6f4";
+      border-color = "#89b4fa";
 
-    defaultTimeout = 5000;
+      default-timeout = 5000;
 
-    extraConfig = ''
-      [urgency=low]
-      border-color=#a6adc8
+      "urgency=low" = {
+        border-color = "#a6adc8";
+      };
 
-      [urgency=normal]
-      border-color=#89b4fa
+      "urgency=normal" = {
+        border-color = "#89b4fa";
+      };
 
-      [urgency=high]
-      border-color=#f38ba8
-      default-timeout=0
-    '';
+      "urgency=high" = {
+        border-color = "#f38ba8";
+        default-timeout = 0;
+      };
+    };
   };
 }
