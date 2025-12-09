8138a6c044fdc75b1267ac73cd3d221ceaf6d673
Braden Mars <bradenmars@bradenmars.me>
2025-12-08 18:26:48 -0600

    feat(hosts/braden-serval-ws): add hardware configuration
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ hosts/braden-serval-ws/hardware.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # enable all redistributable firmware
+  hardware.enableRedistributableFirmware = true;
+  hardware.enableAllFirmware = true;
+}
