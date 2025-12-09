10f7778a6ab6cae2a8aeea9c12a18ce03c089b86
Braden Mars <bradenmars@bradenmars.me>
2025-12-09 17:53:38 -0600

    fix(modules/hardware/bluetooth): ble devices not working
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -3,6 +3,12 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        KernelExperimental = true;
+      };
+    };
   };
 
   services.blueman.enable = true;
