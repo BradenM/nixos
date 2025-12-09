9d0bb24fde4473646de4a0331a4ed2ebb5a7e3e7
Braden Mars <bradenmars@bradenmars.me>
2025-12-09 01:56:30 -0600

    feat(home/scripts/desktop): add desktop scripts from dotfiles
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ home/scripts/desktop.nix
@@ -0,0 +1,76 @@
+{ config, lib, pkgs, inputs, ... }:
+
+let
+  scriptsDir = "${inputs.dotfiles}/dotfiles/scripts";
+  swayScriptsDir = "${inputs.dotfiles}/dotfiles/config/sway/scripts";
+
+  lock = pkgs.writeShellApplication {
+    name = "lock";
+    runtimeInputs = [ pkgs.swaylock ];
+    text = builtins.readFile "${swayScriptsDir}/lock.sh";
+  };
+
+  select-area = pkgs.writeShellApplication {
+    name = "select-area";
+    runtimeInputs = with pkgs; [ sway jq slurp libnotify ];
+    text = builtins.readFile "${scriptsDir}/select-area";
+  };
+
+  sway-prop = pkgs.writeShellApplication {
+    name = "sway-prop";
+    runtimeInputs = with pkgs; [ sway jq kitty ];
+    text = builtins.readFile "${scriptsDir}/sway-prop";
+  };
+
+  swytch = pkgs.writeShellApplication {
+    name = "swytch";
+    runtimeInputs = with pkgs; [ sway jq wofi gawk coreutils ];
+    text = builtins.readFile "${scriptsDir}/swytch";
+  };
+
+  screenrec = pkgs.writeShellApplication {
+    name = "screenrec";
+    runtimeInputs = with pkgs; [ sway wf-recorder coreutils ];
+    text = builtins.replaceStrings
+      [ "$(~/.scripts/select-area)" ]
+      [ "$(${select-area}/bin/select-area)" ]
+      (builtins.readFile "${swayScriptsDir}/screenrec.sh");
+  };
+
+  stopscreenrec = pkgs.writeShellApplication {
+    name = "stopscreenrec";
+    runtimeInputs = with pkgs; [ procps ];
+    text = builtins.readFile "${swayScriptsDir}/stopscreenrec.sh";
+  };
+
+  autotiling = pkgs.writeShellApplication {
+    name = "autotiling";
+    runtimeInputs = [ pkgs.autotiling ];
+    text = builtins.readFile "${swayScriptsDir}/autotiling.sh";
+  };
+
+  import-gsettings = pkgs.writeShellApplication {
+    name = "import-gsettings";
+    runtimeInputs = with pkgs; [ gnome-settings-daemon glib gnused ];
+    text = builtins.readFile "${scriptsDir}/import-gsettings";
+  };
+
+  electron-wayland = pkgs.writeShellApplication {
+    name = "electron-wayland";
+    runtimeInputs = [ ];
+    text = builtins.readFile "${scriptsDir}/electron-wayland";
+  };
+in
+{
+  home.packages = [
+    lock
+    select-area
+    sway-prop
+    swytch
+    screenrec
+    stopscreenrec
+    autotiling
+    import-gsettings
+    electron-wayland
+  ];
+}
