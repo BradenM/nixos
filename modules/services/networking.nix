5d2a9ee297b2dd3cbe889941828aae6c2b42569a
Braden Mars <bradenmars@bradenmars.me>
2025-12-08 18:22:51 -0600

    feat(modules/services/networking): setup openssh, persist keys
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- modules/services/networking.nix
+++ modules/services/networking.nix
@@ -5,8 +5,27 @@
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ ];
+    allowedTCPPorts = [ 22 ];
     allowedUDPPorts = [ ];
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "prohibit-password";
+      PasswordAuthentication = true;
+    };
+    hostKeys = [
+      {
+        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/persist/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
+      }
+    ];
   };
 
   # mDNS/DNS-SD
