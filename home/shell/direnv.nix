527c5acbf7e74a3a1b12b9e63b0e0df335cfcb80
Braden Mars <bradenmars@bradenmars.me>
2026-04-27 15:11:34 -0500

    feat(home/shell/direnv)P: add export function
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- home/shell/direnv.nix
+++ home/shell/direnv.nix
@@ -17,7 +17,32 @@ in
       };
     };
 
-    stdlib = direnvRcContent;
+    # stdlib = direnvRcContent;
+    stdlib = ''
+      ${direnvRcContent}
+
+# Export a function in direnv environment.
+# Extension of: https://github.com/direnv/direnv/issues/73#issuecomment-152284914
+# with support for specifying execution shell
+export_function() {
+	local name=$1
+	local sh="${"$"}{2:-$SHELL}"
+	local alias_dir="$(direnv_layout_dir)/aliases"
+	mkdir -p "$alias_dir"
+	PATH_add "$alias_dir"
+	local target="$alias_dir/$name"
+	if declare -f "$name" >/dev/null; then
+		#echo "#!$SHELL" >"$target"
+		echo "#!/usr/bin/env $(basename "$sh")" >"$target"
+		declare -f "$name" >>"$target" 2>/dev/null
+		# Notice that we add shell variables to the function trigger.
+		echo "$name \$*" >>"$target"
+		chmod +x "$target"
+	fi
+}
+
+      
+    '';
 
   };
 }
