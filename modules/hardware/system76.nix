587a713cb65ee2d1ffbe4700597f62969bb809de
Braden Mars <bradenmars@bradenmars.me>
2026-05-17 01:36:23 -0500

    feat(hardware/system76): enable fwupd, bolt
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- modules/hardware/system76.nix
+++ modules/hardware/system76.nix
@@ -8,6 +8,12 @@
   # disable conflicting power management
   services.power-profiles-daemon.enable = false;
   services.tlp.enable = false;
+  
+  # firmware updates
+  services.fwupd.enable = true;
+
+  # thunderbold daemon
+  services.hardware.bolt.enable = true;
 
   environment.systemPackages = with pkgs; [
     firmware-manager
